0bc1564da49c5377f0eb1eda952c0989b0685f3c
Mohammad Al Issa <mohammed.alissa627@gmail.com>
2023-10-11 02:36:57 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+  const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+  function superbowlWin(arr) {
+    const winningYear = arr.find(element => element.result === 'W');
+    return winningYear ? winningYear.year : undefined;
+  }
+  
+console.log(superbowlWin(sadReality))
